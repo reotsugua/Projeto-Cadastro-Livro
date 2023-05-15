86e44dd43a1f800df7b2f3d67f5a82cbff241365
reotsugua <renanaugusto.ras@gmail.com>
2023-05-15 20:23:04 -0300

    repository

---
+++ biblioteka/src/main/java/com/example/biblioteka/repository/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.example.biblioteka.repository;
+
+import com.example.biblioteka.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+}
+
